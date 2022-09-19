a0ba17371225d35bc12528992f71969d7f8351fa
DippyArtu <artur.makhnach@gmail.com>
2022-09-19 16:26:29 +0300

    deployer scripts done

--- packages/hardhat/deploy/00_deploy_contact.js
+++ packages/hardhat/deploy/00_deploy_contact.js
@@ -6,11 +6,20 @@
 const           { ethers } = require("hardhat");
 const           localChainId = "31337";
 
-const           USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955"; // <---- USDT address BSC mainnet
-//const           USDT_ADDRESS = "0x377533D0E68A22CF180205e9c9ed980f74bc5050"; // <---- USDT address BSC testnet
+const           DEPLOY = 0    // <------- 0 — initial deploy (token + round)
+                              //          1 — round deploy
+const           TRANSFER_OWNERSHIP = 1; // <------ 0 — yes
+                                        //         1 — no
+
+//const           USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955"; // <---- USDT address BSC mainnet
+const           USDT_ADDRESS = "0x377533D0E68A22CF180205e9c9ed980f74bc5050"; // <---- USDT address BSC testnet
+
+const           TOKEN_ADDRESS = "0x00"; // <----- token address needed for round deploy (after initial)
+
 //const           DEV_ADDRESS = "0x95e9450e2737e2239Be1CE225D79E4B2bE171f71"; // <----- dev address EOA
 //const           DEV_ADDRESS = "0x6a8bf9f647d920a3f00470c313542088ad808285"; // <----- dev address hardhat
 const           DEV_ADDRESS = "0xc5F1a117838631225a85863C440223bd25dfD7b3"; // <----- dev address hardhat front
+
 const           ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
 const           TOKEN_CONTRACT_NAME = "ShelterzToken"; // <--- specify contract names
 
@@ -19,62 +28,114 @@ const           CONTRACTS = [TOKEN_CONTRACT_NAME,
                              "Round"];                // <--- specify contract names
 const           numContracts = CONTRACTS.length;
 
-module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const chainId = await getChainId();
 
-  for (i = 0; i < numContracts; i += 1) {
+
+
+
+
+
+
+
+
+if (DEPLOY == 0) {
+  module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = await getChainId();
+  
+    for (i = 0; i < numContracts; i += 1) {
+      console.log("🌐 Deploying...");
+      if (CONTRACTS[i] == "Round") { // <--------------------- contracts with arguments
+        let TOKEN = await ethers.getContract(CONTRACTS[0], deployer);
+        TOKEN = TOKEN.address;
+  
+        let USDT = await ethers.getContract(CONTRACTS[1], deployer); // <--- comment out for production
+        USDT = USDT.address; // <--- comment out for production
+        // USDT = USDT_ADDRESS; // <--- enable for production
+  
+        await deploy(CONTRACTS[i], {
+          from: deployer,
+          args: [TOKEN, USDT],
+          log: true,
+          waitConfirmations: 5,
+        });
+      }
+      else {
+        await deploy(CONTRACTS[i], { // <--------------------- contracts with no arguments
+          from: deployer,
+          args: [],
+          log: true,
+          waitConfirmations: 5,
+        });
+      }
+    }
+    console.log("========= 📡 Contracts deployed! =========");
+  
+    if (TRANSFER_OWNERSHIP == 0) {
+      // Transfer contract ownership to dev EOA
+      //
+      let Contract;
+      for (i = 0; i < numContracts; i += 1) {
+        Contract = await ethers.getContract(CONTRACTS[i], deployer);
+        if (CONTRACTS[i] == TOKEN_CONTRACT_NAME) {
+          await Contract.grantRole(ADMIN_ROLE, DEV_ADDRESS);
+          await Contract.revokeRole(ADMIN_ROLE, deployer);
+        }
+        else {
+          await Contract.transferOwnership(DEV_ADDRESS);
+        }
+        console.log("❗️ |", CONTRACTS[i], "| Ownership transfered to: ", DEV_ADDRESS);
+      }
+    }
+  
+    // Approve dev address USDT for spend
+    //
+    // let USDT = await ethers.getContract(CONTRACTS[1], deployer); // <--- comment out for production
+    // // USDT = USDT_ADDRESS; // <--- enable for production
+  
+    // let ROUND = await ethers.getContract(CONTRACTS[2], deployer);
+    // await USDT.approve(ROUND.address, ethers.utils.parseEther("200000000"));
+    // console.log("❗️ | Allowance for USDT updated!");
+  };
+  module.exports.tags = [CONTRACTS];
+}
+
+
+
+else if (DEPLOY == 1) {
+  module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = await getChainId();
+  
     console.log("🌐 Deploying...");
-    if (CONTRACTS[i] == "Round") { // <--------------------- contracts with arguments
-      let TOKEN = await ethers.getContract(CONTRACTS[0], deployer);
-      TOKEN = TOKEN.address;
+    TOKEN = TOKEN_ADDRESS;
+    USDT = USDT_ADDRESS;
 
-      let USDT = await ethers.getContract(CONTRACTS[1], deployer); // <--- comment out for production
-      USDT = USDT.address; // <--- comment out for production
-      // USDT = USDT_ADDRESS; // <--- enable for production
-
-      await deploy(CONTRACTS[i], {
-        from: deployer,
-        args: [TOKEN, USDT],
-        log: true,
-        waitConfirmations: 5,
-      });
+    await deploy(CONTRACTS[2], {
+      from: deployer,
+      args: [TOKEN, USDT],
+      log: true,
+      waitConfirmations: 5,
+    });
+    console.log("========= 📡 Contracts deployed! =========");
+  
+    if (TRANSFER_OWNERSHIP == 0) {
+      // Transfer contract ownership to dev EOA
+      //
+      let Contract;
+      for (i = 0; i < numContracts; i += 1) {
+        Contract = await ethers.getContract(CONTRACTS[2], deployer);
+        if (CONTRACTS[i] == TOKEN_CONTRACT_NAME) {
+          await Contract.grantRole(ADMIN_ROLE, DEV_ADDRESS);
+          await Contract.revokeRole(ADMIN_ROLE, deployer);
+        }
+        else {
+          await Contract.transferOwnership(DEV_ADDRESS);
+        }
+        console.log("❗️ |", CONTRACTS[2], "| Ownership transfered to: ", DEV_ADDRESS);
+      }
     }
-    else {
-      await deploy(CONTRACTS[i], { // <--------------------- contracts with no arguments
-        from: deployer,
-        args: [],
-        log: true,
-        waitConfirmations: 5,
-      });
-    }
-  }
-  console.log("========= 📡 Contracts deployed! =========");
-
-  // Transfer contract ownership to dev EOA
-  //
-  let Contract;
-  for (i = 0; i < numContracts; i += 1) {
-    Contract = await ethers.getContract(CONTRACTS[i], deployer);
-    if (CONTRACTS[i] == TOKEN_CONTRACT_NAME) {
-      await Contract.grantRole(ADMIN_ROLE, DEV_ADDRESS);
-      await Contract.revokeRole(ADMIN_ROLE, deployer);
-    }
-    else {
-      await Contract.transferOwnership(DEV_ADDRESS);
-    }
-    console.log("❗️ |", CONTRACTS[i], "| Ownership transfered to: ", DEV_ADDRESS);
-  }
-
-  // Approve dev address USDT for spend
-  //
-  let USDT = await ethers.getContract(CONTRACTS[1], deployer); // <--- comment out for production
-  // USDT = USDT_ADDRESS; // <--- enable for production
-
-  let ROUND = await ethers.getContract(CONTRACTS[2], deployer); // <--- comment out for production
-
-  await USDT.approve(ROUND.address, ethers.utils.parseEther("200000000"));
-  console.log("❗️ | Allowance for USDT updated!");
-};
-module.exports.tags = [CONTRACTS];
+  };
+  module.exports.tags = [CONTRACTS];
+}
